f8b80550ea2f009912ca6dfb135a0a5f3519fd4f
arghyapandit <arghyapandit@gmail.com>
2021-08-08 13:17:21 +0600

    Values and Variables

---
+++ 01-Fundamentals-Part-1/01.Values and Variables/script.js
@@ -0,0 +1,13 @@
+let myCountry = "Bangladesh";
+let myContinent = "Asia";
+let myPopulation = "180 Million";
+
+//Loging only values of variable
+console.log(myCountry);
+console.log(myContinent);
+console.log(myPopulation);
+
+//Loging values of variable as sentences
+console.log("I am from " + myCountry + ".");
+console.log(myCountry + " is situated in " + myContinent + ".");
+console.log("Bangladesh has almost " + myPopulation + " people.");
